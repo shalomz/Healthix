a3a472f6a92520b0bf9d830eb44c29a938183cb4
shalomz <shalom.nyende@gmail.com>
2018-01-24 15:35:00 +0300

    Clean Up

--- patients/views.py
+++ patients/views.py
@@ -11,9 +11,10 @@ from django.template.loader import render_to_string
 
 from .forms import PatientRegisterForm
 from .models import Patient
-
+from insurance_companies.models import InsuranceCompany
 
 def new_patient(request):
+    companies = InsuranceCompany.objects.all()
     if request.method == 'POST':
         form = PatientRegisterForm(request.POST or None, files=request.FILES)
         if form.is_valid():
@@ -33,11 +34,18 @@ def new_patient(request):
                 else:
                     new_patient.patient_code = 'HEALTHIX-{}-{}'.format(
                         datetime.datetime.now().year, str(p.id + 1))
+            company = request.POST['insurances']
+            print(company)
+            number = request.POST.get('insurance_number')
+            print(number)
+            ins_comp = InsuranceCompany.objects.get(name=company)
+            new_patient.insurance_company = ins_comp
+            new_patient.insurance_number = number
             new_patient.save()
             return redirect('/')
     else:
         form = PatientRegisterForm()
-    return render(request, 'new_patient.html', {'form': form})
+    return render(request, 'new_patient.html', {'form': form, 'companies': companies})
 
 
 def all_patients(request):
